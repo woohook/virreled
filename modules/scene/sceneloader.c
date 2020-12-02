5e9ee063d8619b070caa2852f39b3519025c576f
woohook <woohook@localhost.net>
2020-12-02 01:08:07 +0100

    Introduced entity loader

--- modules/scene/sceneloader.c
+++ modules/scene/sceneloader.c
@@ -8,6 +8,47 @@ extern float     g_cam_rx, g_cam_ry, g_cam_rz;
 
 void model_load(const char* filename, float x, float y, float z, float rx, float ry, float rz);
 
+void entity_load(const char* filename, float x, float y, float z)
+{
+  FILE* file        = fopen(filename, "r");
+  int   matchcount  = 0;
+
+  if(file == 0)
+  {
+    printf("ERROR: %s could not be opened\n", filename);
+    return;
+  }
+
+  while(1)
+  {
+    char line [1024];
+    line[1023] = 0;
+    matchcount = fscanf(file, "%1023[^\n]", &line);
+    fscanf(file, "\n");
+    if (matchcount == 1)
+    {
+      float x, y, z;
+      float rx = 0, ry = 0, rz = 0;
+      char model_filename[1024];
+      int offset = 0;
+
+      if(4 == sscanf(line, " model %1023[^ ] x=%f y=%f z=%f%n", &model_filename, &x, &y, &z, &offset))
+      {
+        sscanf(&line[offset], " rx=%f ry=%f rz=%f", &rx, &ry, &rz);
+        model_load(model_filename, x, y, z, rx, ry, rz);
+      }
+
+      line[0] = 0;
+    }
+    else if (matchcount == EOF)
+    {
+      break;
+    }
+  }
+
+  fclose(file);
+}
+
 void scene_load(const char* filename)
 {
   FILE* file        = fopen(filename, "r");
@@ -32,7 +73,13 @@ void scene_load(const char* filename)
       float x, y, z;
       float rx = 0, ry = 0, rz = 0;
       char model_filename[1024];
+      char entity_filename[1024];
       int offset = 0;
+
+      if(4 == sscanf(line, " entity %1023[^ ] x=%f y=%f z=%f%n", &entity_filename, &x, &y, &z, &offset))
+      {
+        entity_load(entity_filename, x, y, z);
+      }
 
       if(4 == sscanf(line, " model %1023[^ ] x=%f y=%f z=%f%n", &model_filename, &x, &y, &z, &offset))
       {
